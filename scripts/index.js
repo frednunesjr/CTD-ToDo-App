cd69d4572f39aad2788fd3bd5b5a357d22eda29d
Fred Nunes <fred@multipixel.com.br>
2022-04-10 04:29:08 -0300

    Refatoracao da validacao de login, dados salvos na sessao, condicoes de erros do fetch criados

--- scripts/index.js
+++ scripts/index.js
@@ -1,114 +1,111 @@
-//pega todos os campos com required
-const campos = document.querySelectorAll("[required]");
+const campos	= document.querySelectorAll("[required]");
+const form		= document.querySelector("form");
 
 //funcao que valida os campos e exibe mensagem de obrigatorio
-function validaCampo(campo){
-  //funcao que verifica os erros nos campos (vazio)
-  function verificaErros(){
-    let encontraErro = false;
-
-    for(const erro in campo.validity){
-      if(campo.validity[erro] && !campo.validity.valid){
-        encontraErro = erro;
-      }
-    }
-
-    return encontraErro;
-  }
-
-  //funcao de customizacao da mensagem
-  function customizarMessagem(typeError){
-    //objeto mensagem com os estados de validacao da mensagem
-    const messagens = {
-      password: {
-        valueMissing: "Campo precisa ser preenchido"
-      },
-      email: {
-        valueMissing: "Email é obrigatório",
-        typeMismatch: "Preencha um email válido"
-      }
-    }
-    //retornando o objeto messagens e a chave com type password ou email
-    return messagens[campo.type][typeError]
-  }
-
-  //mensagem customizada da caixa
-  function setMessagem(message = "Campo obrigatorio"){
-    const spanErro = campo.parentNode.querySelector("span.error");
-    //condicional que ativa a mensagem de texto e remove quando estiver preenchido
-    if(message){
-      spanErro.classList.add("active");
-      spanErro.innerHTML = message;
-    } else{
-      spanErro.classList.remove("active");
-      spanErro.innerHTML = "";
-    }
-    
-  }
-  //retorna a funcao para mensagem customizavel
-  return function(){
-
-    if(verificaErros()){
-      const message = customizarMessagem(verificaErros());
-      campo.style.borderColor = "red";
-      setMessagem(message);
-    } else {
-      campo.style.borderColor = "green";
-      setMessagem("");
-    }
-  }
+const invalidListener = function(event){
+	event.preventDefault();
+	
+	//objeto mensagem com os estados de validacao da mensagem
+	let mensagem = {
+		email: {
+			typeMismatch: "Preencha um e-mail válido",
+			patternMismatch: "Preencha um e-mail válido",
+			valueMissing: "Este campo é obrigatório",
+		},
+		password: {
+			patternMismatch: "Sua senha deve conter entre 6 e 10 caracteres",
+			valueMissing: "Este campo é obrigatório",
+		}
+	}
+	
+	//verifica os erros nos campos (vazio)
+	for(let erro in this.validity){
+		
+		let spanErro	= this.parentNode.querySelector(".inputError");
+		if(this.validity[erro] && !this.validity.valid && !spanErro){
+			// campo.setCustomValidity(mensagem[campo.type][erro]);
+			let spanErro 	= document.createElement('span');
+			
+			//mensagem customizada da caixa
+			let textoErro	= document.createTextNode(mensagem[this.type][erro]);
+			spanErro.classList.add('inputError');
+			spanErro.appendChild(textoErro);
+			this.parentNode.appendChild(spanErro);
+			this.style.borderColor = "red";
+		}
+	}
 }
 
-
-//funcao que valida o campo email e senha
-function valida(event){
-  const campo = event.target;
-  const validacao = validaCampo(campo);
-  validacao();
+const limparValidacao = function(){
+	let spanErro = this.parentNode.querySelector(".inputError");
+	if(spanErro)
+	this.parentNode.removeChild(spanErro);
+	this.removeAttribute("style");
 }
 
-for(let campo of campos){
-  campo.addEventListener("invalid", event => {
-    event.preventDefault();
-    valida(event);
-  });
-  campo.addEventListener("blur", valida);
-  
-}
-document.querySelector("form").addEventListener("submit", event => {
-  //console.log("enviar o formulario");
+for(let campo of campos) {
+	["invalid", "blur"].forEach(event => {
+		campo.addEventListener(event, invalidListener);
+	});
+	
+	campo.addEventListener("keydown", limparValidacao);
+} 
 
-  //previne o envio do formulario
-  event.preventDefault();
+form.addEventListener("submit", event => {
+	//console.log("enviar o formulario");
+	
+	//previne o envio do formulario
+	event.preventDefault();
+	
+	let email = document.querySelector("#inputEmail");
+	let senha = document.querySelector("#inputPassword");
+	
+	let dadosUsuario = {
+		email: email.value,
+		password: senha.value
+	}
+	
+	let api = "https://ctd-todo-api.herokuapp.com/v1/users/login";
+	let promessa = fetch(api, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify(dadosUsuario)
+	});
 
-  let email = document.querySelector("#inputEmail");
-  let senha = document.querySelector("#inputPassword");
-
-  let dadosUsuario = {
-    email: email.value,
-    password: senha.value
-  }
-
-  // let api = ;
-  let promessa = fetch("https://ctd-todo-api.herokuapp.com/v1/users/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(dadosUsuario)
-  });
-
-  promessa
-  .then(function(res){
-    return res.json();
-  })
-  .then(function(data){
-    const token = data.jwt;
+	promessa
+	.then((res) => {
+		let status = res.status.toString()[0]
+		switch(status){
+			case "4":
+				throw "Usuário ou Senha Incorretos";
+			case "2":
+				return res.json();
+			default:
+				throw "Ocorreu um erro inesperado, tente novamente em instantes";
+		}
+	})
+	.then((data) => {
+		if(data) {
+			const token = data.jwt;
 			localStorage.setItem('token', token);
-			console.log(data)
-  })
-  .catch(function(err){
-    console.log(err)
-  })
-
+			window.location.href = "./tarefas.html";
+		} else {
+			throw "Ocorreu um erro inesperado, tente novamente em instantes";
+		}
+	})
+	.catch((err) => {
+		let spanErro	= form.querySelector(".inputError");
+		
+		if(!spanErro) {
+			let spanErro 	= document.createElement('span');
+			let textoErro	= document.createTextNode(err);
+			spanErro.classList.add('inputError');
+			spanErro.appendChild(textoErro); 
+			form.appendChild(spanErro);
+		} else {
+			spanErro.textContent = err;
+		}
+	});
 })
